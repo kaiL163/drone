0960b92137ee6a1300cbedefedc390991e47b165
qrine <kayzerov444@gmail.com>
2026-02-09 15:55:32 +0400

    123

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import robomaster
-import cv2
 from robomaster import robot
 
 
@@ -8,33 +7,15 @@ if __name__ == "__main__":
     tl_drone = robot.Drone()
     tl_drone.initialize()
 
-    version = tl_drone.get_sdk_version()
-    print("Версия дрончика: {0}".format(version))
-
-    tl_battery = tl_drone.battery
-    battery_info = tl_battery.get_battery()
-    print("Заряд батарейки: {0}".format(battery_info))
-
     tl_flight = tl_drone.flight
-    tl_flight.takeoff().wait_for_completed()
-    tl_flight.land().wait_for_completed()
 
-    tl_camera = tl_drone.camera
-    tl_camera.start_video_stream(display=False)
-    tl_camera.set_fps("high")
-    tl_camera.set_resolution("high")
-    tl_camera.set_bitrate(6)
-    
-    for i in range(0, 302):
-        img = tl_camera.read_cv2_image()
-        if img is not None:
-            cv2.imshow("Дрон", img)
-            cv2.waitKey(1)
-        else:
-            print(f"Предупреждение: Не удалось прочитать кадр на итерации {i}")
-    
-    cv2.destroyAllWindows()
-    tl_camera.stop_video_stream()
+    tl_flight.up(distance=100).wait_for_completed()
 
+    tl_flight.forward(distance=100).wait_for_completed()
+    tl_flight.right(distance=100).wait_for_completed()
+    tl_flight.back(distance=100).wait_for_completed()
+    tl_flight.left(distance=100).wait_for_completed()
+
+    tl_flight.down(distance=100).wait_for_completed()
 
     tl_drone.close()
